fcf26f8537fe83be85580ae30d3a5ea4ebb4d94d
lyricalboii <lyricalboii001@gmail.com>
2023-03-31 15:29:32 +0530

    version1

--- conn.js
+++ conn.js
@@ -1,14 +1,11 @@
 const mongoose = require("mongoose");
-const DB = "mongodb+srv://renish:renidh4707@cluster0.qiyfmu6.mongodb.net/?retryWrites=true&w=majority";
+const DB = "mongodb+srv://renish:renidh6565@cluster0.qiyfmu6.mongodb.net/?retryWrites=true&w=majority";
 
 // const DB = "mongodb+srv://aggregateagro:WNz2B0yMLJIxWccR@cluster0.1dwperj.mongodb.net/?retryWrites=true&w=majority";f
 
 mongoose.set("strictQuery", false);
 mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(DB)
   .then(() => {
     console.log("connection Start");
   })
